539fce3bd7bf3abc051a515053f763c24325ed55
Luca Guidi <me@lucaguidi.com>
2017-11-10 17:14:17 +0100

    Make it work with h2o

--- config/early_hints.rb
+++ config/early_hints.rb
@@ -7,14 +7,21 @@ module Hanami
     end
 
     def call(env)
-      response = @app.call(env)
+      @app.call(env).tap do
+        send_early_hints(env)
+      end
+    end
 
-      link = Thread.current[:__hanami_assets].map do |asset|
-        "<#{asset}>; rel=preload"
-      end.join("\n")
+    private
 
-      env["rack.early_hints"].call("Link" => link)
-      response
+    def send_early_hints(env)
+      Thread.current[:__hanami_assets].each_slice(10) do |slice|
+        link = slice.map do |asset|
+          "<#{asset}>; rel=preload"
+        end.join("\n")
+
+        env["rack.early_hints"].call("Link" => link)
+      end
     end
   end
 end
