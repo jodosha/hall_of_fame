3c30232199f74cd6f4e01e707854fee3dd71ab75
Luca Guidi <me@lucaguidi.com>
2017-11-10 17:21:24 +0100

    Nicer UI

--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -1,5 +1,11 @@
 module Web::Views::Home
   class Index
     include Web::View
+
+    def avatar(contributor)
+      link_to "https://github.com/#{contributor.username}", target: "_blank" do
+        image(contributor.avatar_url, alt: contributor.username, title: contributor.username, width: "200", height: "200")
+      end
+    end
   end
 end
